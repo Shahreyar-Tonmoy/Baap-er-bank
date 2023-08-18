2fe31016fa8818f915183d1da90136f83693edb0
Shahreyar Tonmoy <shahreyartonmoy001@gmail.com>
2023-08-18 18:08:34 +0600

    fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const submit = document.getElementById('submit-btn').addEventListener('click', f
     const passwordField = document.getElementById('user-password')
     const password = passwordField.value
     console.log(email, password)
-    if (email === 'Shahreyartonmoy001@gmail.com' && password === 'Tonmoy#001', email=== 'Tonmoymspc@gmail.com' && password=== 'Tonmoy#001' ){
+    if (email === 'Shahreyartonmoy001@gmail.com' && password === 'Tonmoy#001'){
         window.location.href = 'bank.html'
     }
     else if (email=== 'Tonmoymspc@gmail.com' && password=== 'Tonmoy#001'){
